e1618e3ee4bf22ded87bb0aa892080bb29cebe42
Phibian <phibdev@gmail.com>
2024-11-07 16:24:15 -0500

    Added enum helper methods

--- Util/EnumUtil.cs
+++ Util/EnumUtil.cs
@@ -8,4 +8,19 @@ public static class EnumUtil {
     public static IEnumerable<T> GetValues<T>() {
         return Enum.GetValues(typeof(T)).Cast<T>();
     }
+
+    public static int CountValues<T>()
+    {
+        return Enum.GetValues(typeof(T)).Length;
+    }
+
+    public static T Next<T>(this T value) where T : Enum
+    {
+        return (T)Enum.ToObject(typeof(T), (value.ToInt() + 1) % CountValues<T>());
+    }
+    
+    public static int ToInt(this Enum a)
+    {
+        return Convert.ToInt32(a);
+    }
 }

92d220d03a967afec4bc5668e2d1e0831456af92
Phibian <phibdev@gmail.com>
2024-11-15 10:12:34 -0500

    Added ToVector2() Direction4 extension

---
+++ Util/Direction4Extensions.cs
@@ -0,0 +1,20 @@
+using System;
+using Microsoft.Xna.Framework;
+using ViewportGame;
+
+namespace ViewportEngine.Util;
+
+public static class Direction4Extensions
+{
+    public static Vector2 ToVector2(this Direction4 direction)
+    {
+        return direction switch
+        {
+            Direction4.Down => new Vector2(0, -1),
+            Direction4.Up => new Vector2(0, 1),
+            Direction4.Right => new Vector2(1, 0),
+            Direction4.Left => new Vector2(-1, 0),
+            _ => new Vector2(0, -1)
+        };
+    }
+}

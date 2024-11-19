01c3a284b268965e990610ba3743b9f7c249ed05
Phibian <phibdev@gmail.com>
2024-11-19 13:30:45 -0500

    Added pixel perfect scaling to VPEGame

--- Util/IWindowHandler.cs
+++ Util/IWindowHandler.cs
@@ -4,8 +4,8 @@ namespace ViewportEngine.Util;
 
 public interface IWindowHandler
 {
-    public int Width { get; }
-    public int Height { get; }
+    public int WindowWidth { get; }
+    public int WindowHeight { get; }
     public Point Size { get; }
 
     public Point Position { get; }
@@ -13,6 +13,4 @@ public interface IWindowHandler
     
     void SetWindowSize(int width, int height);
     void SetWindowPosition(int x, int y);
-
-    public void SetRenderScale(int multiplier);
 }
--- VPEGame.cs
+++ VPEGame.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using ViewportEngine.Util;
@@ -9,17 +10,30 @@ public abstract class VPEGame : Game, IWindowHandler
     protected GraphicsDeviceManager Graphics { get; private set; }
     protected SpriteBatch SpriteBatch { get; private set; }
 
-    public int Width => Graphics.PreferredBackBufferWidth;
-    public int Height => Graphics.PreferredBackBufferHeight;
+    public int WindowWidth => Graphics.PreferredBackBufferWidth;
+    public int WindowHeight => Graphics.PreferredBackBufferHeight;
+    
+    public abstract int PixelWidth { get; }
+    public abstract int PixelHeight { get; }
+    
+    public int PixelScale { get; private set; }
+    
     public Point Size => Window.ClientBounds.Size;
     public Point Position => Window.Position;
-    public Point CenterPosition => Position + new Point(Width / 2, Height / 2);
+    public Point CenterPosition => Position + new Point(WindowWidth / 2, WindowHeight / 2);
 
+    protected Rectangle WindowCenteredRect
+    {
+        get;
+        private set;
+    }
 
     protected VPEGame()
     {
         Graphics = new GraphicsDeviceManager(this);
         Content.RootDirectory = "Content";
+        Window.ClientSizeChanged += OnResize;
+        SpriteBatch = null;
     }
 
     protected override void LoadContent()
@@ -29,26 +43,39 @@ public abstract class VPEGame : Game, IWindowHandler
         base.LoadContent();
     }
 
-    protected SpriteBatch GetSpriteBatch()
-    {
-        return SpriteBatch;
-    }
-
     public void SetWindowSize(int width, int height)
     {
         Graphics.PreferredBackBufferWidth = width;
         Graphics.PreferredBackBufferHeight = height;
         Graphics.ApplyChanges();
+        OnWindowSizeChanged(width, height);
+    }
+    
+    private void OnResize(object obj, EventArgs args)
+    {
+        var newScreenWidth = GraphicsDevice.PresentationParameters.BackBufferWidth;
+        var newScreenHeight = GraphicsDevice.PresentationParameters.BackBufferHeight;
+
+        OnWindowSizeChanged(newScreenWidth, newScreenHeight);
+    }
+    
+    private void OnWindowSizeChanged(int newWidth, int newHeight)
+    {
+        PixelScale = Math.Min(newWidth / PixelWidth, newHeight / PixelHeight);
+        
+        var frameWidth = PixelWidth * PixelScale;
+        var frameHeight = PixelHeight * PixelScale;
+        WindowCenteredRect = new Rectangle((newWidth - frameWidth) / 2, (newHeight - frameHeight) / 2, frameWidth, frameHeight);
+    }
+
+    protected static RenderTarget2D GetPixelPerfectRenderTarget(GraphicsDevice gd, int width, int height)
+    {
+        return new RenderTarget2D(gd, width, height, false,
+            gd.PresentationParameters.BackBufferFormat, DepthFormat.Depth24);
     }
 
     public void SetWindowPosition(int x, int y)
     {
         Window.Position = new Point(x, y);
-    }
-
-    public void SetRenderScale(int multiplier)
-    {
-        var newSize = Dimensions * multiplier;
-        SetWindowSize((int)newSize.X, (int)newSize.Y);
     }
 }
